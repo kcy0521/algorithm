b09b82682b9cc3c6288885daba4f71478382f1a5
kcy0521 <kyle.kcy@gmail.com>
2023-02-15 23:56:53 +0900

    백준

--- 백준/1436(영화감독 솜).py
+++ 백준/1436(영화감독 솜).py
@@ -1,9 +1,10 @@
-# n = int(input())
+n = int(input())
 
 # name = str(n-1) + '666'
 # print(name)
 
-for i in range(666,10000000):
-    # 연속된 666이 있는지 확인하기
-    # 있으면 추가한다.  
-    pass
+# cnt = 1
+# while cnt < n :
+#     x - 666
+#     if str(x)
+
---
+++ 백준/9095(1,2,3더하기).py
@@ -0,0 +1,11 @@
+def solution(x):
+    
+    return y
+
+# 갯수로만 나타낸다? 그럼 
+
+n = int(input())
+
+for i in range(n):
+    
+    pass
