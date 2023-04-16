a7e4cd0430f65055b369b51dbc52cb63e3af3b0c
kcy0521 <kyle.kcy@gmail.com>
2023-04-16 21:10:39 +0900

    mm

--- 백준/11723(집합).py
+++ 백준/11723(집합).py
@@ -1,36 +1,37 @@
-def add(x):
-    if x not in S:
-        S.append(x)
-
-def check(x):
-    if x in S:
-        a = S.index(x)
-        S.pop(a)
-
+import sys
 n = int(input())
 
 S = []
-A = [input().split() for _ in range(n)]
+Z = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
 
-for i in range(n):
-    x = A[i][0]
-    if x == 'add':
-        add(A[i][1])
-    elif x == 'remove':
-        check(A[i][1])
-    elif x == 'check':
-        if A[i][1] in S:
-            print(1)
+for _ in range(n):
+    x = list(sys.stdin.readline().split())
+
+    if x[0] == 'add':
+        if x[1] not in S:
+            S.append(x[1])
+
+    elif x[0] == 'remove':
+        if x[1] in S:
+            S.pop(S.index(x[1]))
+
+    elif x[0] == 'check':
+        if x[1] in S:
+            print('1')
         else:
-            print(0)
-    elif x == 'toggle':
-        if A[i][1] in S:
-          x = S.index(A[i][1])
-          S.pop(x)
+            print('0')
+
+    elif x[0] == 'toggle':
+        if x[1] in S:
+            S.pop(S.index(x[1]))
         else:
-          S.append(A[i][1])
-    elif x == 'all':
-        S = list(range(1,21))
-    
-    elif x == 'empty':
+            S.append(x[1])
+
+    elif x[0] == 'all':
+        print(S)
+        S = Z 
+        print(S)
+    elif x[0] == 'empty':
         S = []
+
+
--- 백준/1966(프린터 큐).py
+++ 백준/1966(프린터 큐).py
@@ -3,35 +3,12 @@ T = int(input())
 for tc in range(T):
     n, m = map(int, input().split())
     arr = list(map(int,input().split()))
-    
-    arr2 = list(set(arr))
-    sol = [0] * n
-    cnt = 1
-    
-    for i in arr2:
-        for j in range(n):
-            if not sol[j] and i == arr[j]:
-                sol[j] = cnt 
-                cnt += 1
 
-            if sol[m] != 0:
-                print(sol)
-                print(sol[m])
-                break            
-        
-    # 현재 문서보다 중요도가 높은 문서가 하나라도 있다면, 
-    # 이문서를 인쇄하지 않고 큐의 가장 뒤에 재배치 한다.
+    arr2 = list(set(arr)).sort() # 중요도 리스트
+    queue = []
+
+    while True:
+    # 주어진 방법에 대해서 생각해
+    # 1. 현재 큐의 가장 앞에 있는 문서의 중요도를 확인한다.abs(2. 나머지 문서들 중 현재 문서보다 중요도가 높은 문서가 하나라도 있다면, 이문서를 인쇄 하지 않고 가장 뒤에 배치)
+    if arr[0] 
     
-
-
-'''
- 중요도를 확인해야함 
- 중요도가 높아야 먼저 인쇄가 된다.
- 위의 식은 순서가 바뀌는게 안나타나게 된다. 시부레 이걸 어떻게 해야 하냐 ㅅㅂ
- 좀 ㅈ같네 
- x = 1
-어떤 중요도들이 있는지 확인해봐라
-arr = sorted()
-순서도 기록을 해 놔야 할거 같은데... 이거 어떻게 해야하냐? 
-셔츠도 입어야 하는디 ㄲㅂ 이거 집에 옷이 없으니 
-'''
