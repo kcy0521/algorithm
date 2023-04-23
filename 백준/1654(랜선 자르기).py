2cf08cf4f6ad3517a35e06e7d1099deb0bba1c58
kcy0521 <kyle.kcy@gmail.com>
2023-04-23 17:40:48 +0900

    1654

--- 백준/1654(랜선 자르기).py
+++ 백준/1654(랜선 자르기).py
@@ -1,19 +1,21 @@
 k, n = map(int, input().split())
 
-# 최대 랜선의 길이를 구하는 프로그램을 작성하시오. 
-
-# 입력을 k개 받아야 한다. 
-line = []
+arr = []
 for i in range(k):
-    line.append(int(input()))
+    x = int(input())
+    arr.append(x)
 
-# n 개로 만들어야하는데...
-'''
-a,b,c,d = 가지고 있는 선의 갯수
-x = 구하고 싶은 길이
-n = 만들어야 하는 갯수
-aa + bb + cc + dd = n
-n * x <= a + b + c + d
-가장 길었을때는 d = x 일때 이다.
-손실된 길이... 이걸 어떻게 표현 해야 할까?
-'''
+start, end = 1, max(arr)
+
+while start <= end:
+    mid = (start + end)//2
+    sol = 0
+    for i in arr:
+        sol += i // mid
+    
+    if sol >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
