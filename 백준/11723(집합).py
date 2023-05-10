94ab9a8e976d619f24be665fd437a4f35625aced
kimchangyoung <kyle.kcy@gmail.com>
2023-05-10 17:52:36 +0900

    11723

--- 백준/11723(집합).py
+++ 백준/11723(집합).py
@@ -1,37 +1,31 @@
-import sys
 n = int(input())
+sol = []
 
-S = []
-Z = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
-
-for _ in range(n):
-    x = list(sys.stdin.readline().split())
-
-    if x[0] == 'add':
-        if x[1] not in S:
-            S.append(x[1])
-
-    elif x[0] == 'remove':
-        if x[1] in S:
-            S.pop(S.index(x[1]))
-
-    elif x[0] == 'check':
-        if x[1] in S:
-            print('1')
-        else:
-            print('0')
-
-    elif x[0] == 'toggle':
-        if x[1] in S:
-            S.pop(S.index(x[1]))
-        else:
-            S.append(x[1])
-
-    elif x[0] == 'all':
-        print(S)
-        S = Z 
-        print(S)
-    elif x[0] == 'empty':
-        S = []
-
-
+for i in range(n):
+    a = list(input().split())
+    if len(a) == 1:
+        x = a[1]
+        if a[0] == 'add':
+            if x not in sol:
+                sol.append(x)
+        elif a[0] == 'remove':
+            if x in sol:
+                sol.pop(sol.index(x))
+            
+        elif a[0] == 'check':
+            if x in sol:
+                print(1)
+            else:
+                print(0) 
+        elif a[0] == 'toggle':
+            if x not in sol:
+                sol.append(x)
+            else:
+                sol.append(x)
+    else: 
+        # all and empty
+        if a[0] == 'all':
+            sol = [range(1,21)]
+        elif a[0] == 'empty':    
+            sol = []
+            
