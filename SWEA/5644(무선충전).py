ab6d3e7d050cdbd7de4bd2b7a7e0f7ff47c9b451
kcy0521 <kyle.kcy@gmail.com>
2023-08-16 20:57:03 +0900

    swea문제풀기

---
+++ SWEA/5644(무선충전).py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for tc in range(1,T+1):
+    N, AP = map(int,input().split())
+
+    A = list(map(int,input().split()))
+    B = list(map(int,input().split()))
+
+    
+    
+    
--- SWEA/5648(원자 소멸 시뮬레이션).py
+++ SWEA/5648(원자 소멸 시뮬레이션).py
@@ -5,17 +5,69 @@ for tc in range(1,T + 1):
     
     arr = [list(map(int,input().split())) for _ in range(N)]
 
-    all = [[0]* 1000 for _ in range(1000)] # 좌표계가 필요하나? 이부분도 문제인데... 
+    sol = 0
     
-    # 하나하나 일일이 확인해봐야 하는가? 이것도 변수인데? 
-    # 아니 이걸 어떻게 알아
-    for i in range(N):
-        for j in range(i,N):
-            arr[i]
-            # 첫번째거와 두번째거 확인가능한가? N의 수가 바뀌어도 되는가? 부딛힌다면 예외 처리는 어떤식으로 해야할까? 
-            # 1. 이동방향을 먼저 확인한다. 같은 위치에 있는 점이 존재하는가? 중간지점이 존재하는가? 만나는 장소가 존재하냐? 
-            # 2. 
+    for i in range(N-1):
+        for j in range(i+1,N):
+            [x1,y1,n1,k1] = arr[i]
+            [x2,y2,n2,k2] = arr[j]
             
-
-
-
+            if x1 == 'x':
+                pass
+            elif arr[i][2] == 0:
+                if x1 > x2 and y1 < y2 and n2 == 3 and abs(x1-x2) == abs(y1-y2):
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+                elif x1 == x2 and y1 < y2 and n2 == 1:
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+                elif x1 < x2 and y1 < y2 and n2 == 2 and abs(x1-x2) == abs(y1-y2):
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+            
+            elif arr[i][2] == 1:
+                if x1 > x2 and y1 > y2 and n2 == 3 and abs(x1-x2) == abs(y1-y2):
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+                elif x1 == x2 and y1 > y2 and n2 == 0:
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+                elif x1 < x2 and y1 > y2 and n2 == 2 and abs(x1-x2) == abs(y1-y2):
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+            
+            elif arr[i][2] == 2:
+                if x1 > x2 and y1 > y2 and n2 == 0 and abs(x1-x2) == abs(y1-y2):
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+                elif x1 > x2 and y1 == y2 and n2 == 3:
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+                elif x1 > x2 and y1 < y2 and n2 == 1 and abs(x1-x2) == abs(y1-y2):
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+            
+            elif arr[i][2] == 3:
+                if x1 < x2 and y1 > y2 and n2 == 0 and abs(x1-x2) == abs(y1-y2):
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+                elif x1 < x2 and y1 == y2 and n2 == 2:
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+                elif x1 < x2 and y1 < y2 and n2 == 1 and abs(x1-x2) == abs(y1-y2):
+                    sol += (k1 + k2)
+                    arr[i] = ['x','x','x','x']
+                    arr[j] = ['x','x','x','x']
+            # print(sol,i)
+    print(f"#{tc} {sol}")
