a269927e75a5415d94ac06fca1ab7caec4494a4b
kcy0521 <kyle.kcy@gmail.com>
2024-03-15 22:30:29 +0900

    240315

--- 백준/9095(1,2,3더하기).py
+++ 백준/9095(1,2,3더하기).py
@@ -1,13 +1,19 @@
 n = int(input())
+# 1 2 3 의 합으로 나타내는 방법의 수를 구하는 프로그램을 만들어라
+# 규칙을 찾아내는 것이 중요했던 문제 매우 중요??
 
-for _ in range(n):
-    a = int(input())
-    cnt = 0
-    while True:
-       x = 0
-       y = 0
-       z = 0
-
-    # 모든 경우의 수를 구해봐라 
-    
-    
+for i in range(n):
+   m = int(input())
+   dp = [0] * (m+1)
+   
+   for i in range(1,m+1):
+      if i == 1:
+         dp[1] = 1
+      elif i == 2:
+         dp[2] = 2
+      elif i == 3:
+         dp[3] = 4
+      else:
+         dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+      
+   print(dp[m])
